ea2b74099d96e1483ed71c6a6f034c9829ae0fe7
joe <eljoe1717@gmail.com>
2026-08-22 17:46:57 +0300

    Fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -33,6 +33,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      { protocol: "https", hostname: "api.noon.codefanz.com" },
     ],
   },
 };
