2583491ae86eaafe2011a4ca91217362a14752be
joe <eljoe1717@gmail.com>
2026-09-03 22:31:15 +0300

    Fix

--- frontend/src/features/noon/shop/search-result/products-grid.tsx
+++ frontend/src/features/noon/shop/search-result/products-grid.tsx
@@ -7,12 +7,11 @@ type Props = {
 
 const ProductsGrid = ({ products }: Props) => {
   return (
-    <div className="flex flex-wrap gap-3 lg:gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 lg:gap-4">
       {products.map((product, index) => (
-        <ProductCard
-          key={`${product.listing_id}-${index}`}
-          productData={product}
-        />
+        <div key={`${product.listing_id}-${index}`} className="[&>div]:w-full">
+          <ProductCard productData={product} />
+        </div>
       ))}
     </div>
   );
