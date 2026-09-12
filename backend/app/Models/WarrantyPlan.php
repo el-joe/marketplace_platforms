8a539835ebd0dfc31bbd5c4aa65c3a03933f5c6d
Joe <eljoe1717@gmail.com>
2026-09-12 11:58:39 +0300

    Fix

--- backend/app/Models/WarrantyPlan.php
+++ backend/app/Models/WarrantyPlan.php
@@ -46,7 +46,7 @@ class WarrantyPlan extends Model
 
     public function getImageUrlAttribute(): ?string
     {
-        return $this->image_path ? Storage::url($this->image_path) : null;
+        return $this->image_path ? Storage::disk('public')->url($this->image_path) : null;
     }
 
     public function category(): BelongsTo
